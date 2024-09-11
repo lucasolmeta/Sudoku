c3bdd384c89606bbb87d452b62c30842b8d79223
lucasolmeta <lolmetab@gmail.com>
2024-09-11 17:36:20 -0500

    final changes

---
+++ functions.mjs
@@ -0,0 +1,202 @@
+window.checkSudoku = checkSudoku;
+window.generateRequest = generateRequest;
+window.confirmButtonPressed = confirmButtonPressed;
+window.exitButtonPressed = exitButtonPressed;
+
+export { initArray }
+export { buildSudoku }
+export { buildPuzzle }
+export { initPuzzleUI }
+export { checkSudoku }
+
+let initGeneration = true;
+
+function initArray (array){
+    
+    for (let row = 0; row < 9; row++) {
+        array[row]=[];
+        for (let col = 0; col < 9; col++) {
+            array[row][col]=0;
+        }
+    }
+
+    return array;
+}
+
+function buildSudoku (){
+
+    initArray(solution);
+    initArray(puzzle);
+
+    let ticks = 0;
+
+    for(let row = 0; row < 9; row++){
+
+        let iterations = 0;
+
+        for(let col = 0; col < 9; col++){
+            let numPlaced = false;
+        
+            while(!numPlaced){
+                var num = Math.floor(Math.random() * 9)+1
+                iterations++;
+                ticks++;
+            
+                if (ticks%10000==0){
+                    for(row = 8; row > 0; row--){
+                        for(col = 8; col > 0; col--){
+                            solution[row][col]=0;
+                        }
+                    }
+                } else if (iterations>80){
+                    for(col = 8; col >-1; col--){
+                        solution[row][col]=0;
+                    }
+                    iterations = 0;
+                } else if(checkValidity(num, row, col)){
+                   solution[row][col]=num;
+                   numPlaced = true;
+                }
+            }
+        }
+    }
+
+    for(let r = 0; r < 9; r++){
+        for (let c = 0; c < 9; c++){
+            puzzle[r][c]=solution[r][c];
+        }
+    }
+
+    buildPuzzle();
+
+    initPuzzleUI();
+}
+
+function buildPuzzle(){
+
+    for(let i = 0; i < 40; i++){
+        let numRemoved = false;
+
+        while(!numRemoved){
+            var row = Math.floor(Math.random() * 9);
+            var col = Math.floor(Math.random() * 9);
+
+            if(puzzle[row][col]!=0){
+                puzzle[row][col]=0;     
+                numRemoved = true;      
+            }
+        }
+    }
+}
+
+function initPuzzleUI(){
+    for(let r = 0; r < 9; r++){
+        for(let c = 0; c < 9; c++){
+            let thisNum = r*9 + c;
+            
+            const input = document.getElementById('input' + thisNum);
+            const cell = document.getElementById('cell' + thisNum);
+            
+            if(puzzle[r][c]==0){
+                input.value="";
+                cell.style.backgroundColor = ('rgb(225,225,225)');
+            } else {
+                input.value = puzzle[r][c];
+                cell.style.backgroundColor = 'rgb(190,190,190)';
+            }
+
+            input.style.color = 'black';
+        }
+    }
+}
+
+function checkSudoku(){
+    for(let r = 0; r < 9; r++){
+        for(let c = 0; c < 9; c++){
+            let thisNum= r*9+c;
+
+            const input = document.getElementById('input' + thisNum);
+
+            puzzle[r][c]=input.value;
+
+            if(puzzle[r][c]==solution[r][c]){
+                input.style.color = 'blue';
+            } else if (puzzle[r][c]==''){
+                input.style.color = 'black';
+            } else {
+                input.style.color = 'red';
+            }
+        }
+    }
+
+    let solved = true;
+
+    for(let r = 0; r < 9; r++){
+        for (let c = 0; c < 9; c++){
+            if(puzzle[r][c]!=solution[r][c]){
+                solved = false;
+            }
+        }
+    }
+
+    if(solved){
+        alert("Congratulations, you solved the sudoku!");
+    }
+}
+
+function checkValidity(num, row, col){
+    
+    for (let r = 0; r < 9; r++){
+        if(solution[r][col] == num){
+            return false;
+        }
+    }
+    
+    for (let c = 0; c < col; c++){
+        if(solution[row][c] == num){
+            return false;
+        }
+    }
+
+    let boxRow = Math.floor(row/3);
+    let boxCol = Math.floor(col/3);
+
+    for(let r = 3*boxRow; r<3*boxRow+3; r++){
+        for(let c = 3*boxCol; c<3*boxCol+3; c++){
+            if(solution[r][c]== num){
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+function generateRequest(){
+
+    if(initGeneration){
+        buildSudoku();
+        initGeneration=false;
+        return;
+    }
+    
+    let alert = document.getElementById("alert");
+
+    alert.style.display = 'flex';
+}
+
+function confirmButtonPressed (){
+
+    buildSudoku();
+
+    let alert = document.getElementById("alert");
+
+    alert.style.display = 'none';
+}
+
+function exitButtonPressed (){
+
+    let alert = document.getElementById("alert");
+
+    alert.style.display = 'none';
+}
--- script.mjs
+++ script.mjs
@@ -1,117 +1,25 @@
-window.regenerateButtonPressed = regenerateButtonPressed;
-window.generateSudoku = generateSudoku;
-window.checkSudoku = checkSudoku;
-window.confirmButtonPressed = confirmButtonPressed;
-window.exitButtonPressed = exitButtonPressed;
-
-export { generateSudoku }
-
-import { initArray } from "./buildSudoku.mjs";
-import { buildSudoku } from "./buildSudoku.mjs";
-import { resetArray } from "./buildSudoku.mjs";
-import { printSudoku } from "./buildSudoku.mjs";
-import { buildPuzzle } from "./buildSudoku.mjs";
+import { initArray } from "./functions.mjs";
+import { buildSudoku } from "./functions.mjs";
+import { initPuzzleUI } from "./functions.mjs";
+import { buildPuzzle } from "./functions.mjs";
 
 let solution = [];
 let puzzle = [];
 
+window['solution'] = solution;
+window['puzzle'] = puzzle;
+
 initArray(solution);
 initArray(puzzle);
 
-let totalSudokusPlayed = 0;
+buildSudoku();
 
-function regenerateButtonPressed(){
-    totalSudokusPlayed++;
-
-    if(totalSudokusPlayed==1){
-        generateSudoku();
-        return;
-    }
-       
-    let alert = document.getElementById("alert");
-
-    alert.style.display = 'flex';
-}
-
-function generateSudoku(){
-
-    resetArray(solution);
-    resetArray(puzzle);
-
-    buildSudoku(solution);
-
-    for(let r = 0; r < 9; r++){
-        for (let c = 0; c < 9; c++){
-            puzzle[r][c]=solution[r][c];
-        }
-    }
-
-    buildPuzzle(puzzle);
-
-    for(let r = 0; r < 9; r++){
-        for(let c = 0; c < 9; c++){
-            let thisNum = r*9 + c;
-            
-            const input = document.getElementById('input' + thisNum);
-            const cell = document.getElementById('cell' + thisNum);
-            
-            if(puzzle[r][c]==0){
-                input.value="";
-                cell.style.backgroundColor = ('rgb(225,225,225)');
-            } else {
-                input.value = puzzle[r][c];
-                cell.style.backgroundColor = 'rgb(190,190,190)';
-            }
-
-            input.style.color = 'black';
-        }
+for(let r = 0; r < 9; r++){
+    for (let c = 0; c < 9; c++){
+        puzzle[r][c]=solution[r][c];
     }
 }
 
-function checkSudoku(){
-    for(let r = 0; r < 9; r++){
-        for(let c = 0; c < 9; c++){
-            let thisNum= r*9+c;
+buildPuzzle();
 
-            const input = document.getElementById('input' + thisNum);
-
-            puzzle[r][c]=input.value;
-
-            if(puzzle[r][c]==solution[r][c]){
-                input.style.color = 'blue';
-            } else if (puzzle[r][c]==''){
-                input.style.color = 'black';
-            } else {
-                input.style.color = 'red';
-            }
-        }
-    }
-
-    let solved = true;
-
-    for(let r = 0; r < 9; r++){
-        for (let c = 0; c < 9; c++){
-            if(puzzle[r][c]!=solution[r][c]){
-                solved = false;
-            }
-        }
-    }
-
-    if(solved){
-        alert("Congratulations, You Solved The Sudoku!");
-    }
-}
-
-function confirmButtonPressed (){
-    generateSudoku();
-
-    let alert = document.getElementById("alert");
-
-    alert.style.display = 'none';
-}
-
-function exitButtonPressed (){
-    let alert = document.getElementById("alert");
-
-    alert.style.display = 'none';
-}
+initPuzzleUI();
